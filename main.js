c6c3e26b3b213faaef64d6ab2dfb9edb92bdb151
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-05-05 12:19:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,13 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = function(){
-        tocaSom('#som_tecla_pom');
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
     }
+
     contador += 1;
 }
