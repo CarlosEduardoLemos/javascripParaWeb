a566758be31238d9caf0bca3fc461c4ed4210824
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-05-05 13:03:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,4 +12,14 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
+
+    tecla.onkeydown = function(evento){
+        if(evento.code ==='Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa')
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
